96422290801811dfbbfd3148c29cad37369641b2
mateus <demartinomateus@gmail.com>
2025-04-23 01:27:42 -0300

    alterando consigurações de rotas e vite.config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Admin } from "./pages/admin";
 import { Login } from "./pages/login";
@@ -7,7 +7,7 @@ import { Networks } from "./pages/networks";
 import { Private } from "./routes/Private";
 import { ErrorPage } from "./pages/error";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
 
   {
     path: '/',
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,5 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
-  // base:'./',
-  base: "/Dev_Link"
-})
+  base:'./',
+  })
