b3b95ff85689d97f9b9a412ea9805a077969423a
mateus <demartinomateus@gmail.com>
2025-04-23 01:09:20 -0300

    alterando configurações de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Admin } from "./pages/admin";
 import { Login } from "./pages/login";
@@ -7,7 +7,7 @@ import { Networks } from "./pages/networks";
 import { Private } from "./routes/Private";
 import { ErrorPage } from "./pages/error";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
 
   {
     path: '/',
